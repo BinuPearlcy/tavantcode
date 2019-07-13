ddc4efab8d56b3c0a98e631a93bb5325471068af
Admin <Admin@Admin-PC>
2019-07-13 07:08:15 +0530

    first commit
    uploading all files

---
+++ stringhelper/src/stringhelper/stringhelper.java
@@ -0,0 +1,49 @@
+package stringhelper;
+
+ public class stringhelper {
+
+	public static void main (String [] args)
+	{
+	
+	
+	 int []number= {1,2,3,4,5,6,7,8,9,10};
+	 char i ;
+	 int sum=0,sum1=0;
+	 
+	for(i=0;i<=10;i++)
+	{
+		System.out.print("i");
+		if(i%2==0)
+		{
+			
+			sum=sum+i;
+			
+		}
+		else
+		{
+			sum1=sum1+i;
+		}
+		System.out.println("sum of even number:"+sum);
+		System.out.println("sum of odd numbers:"+sum1);
+	}
+}
+
+	public Object sumOfOddAndSumOfEvenNumber(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}	
+			
+
+}
+
+				
+			
+			
+			
+				
+			
+		
+				
+	
+
+
