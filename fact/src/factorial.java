861edf34429b25a9e09c5f9a3b24deefe8da6796
Admin <Admin@Admin-PC>
2019-07-13 06:48:48 +0530

    first commit
    uploading all files

---
+++ fact/src/factorial.java
@@ -0,0 +1,18 @@
+
+public class factorial {
+	
+public static void main(String []args)
+	{
+		int number=7;
+		 int factorial=1;
+		 
+for(int i=1;i<=number;++i) {
+	factorial*=i;
+}	
+	System.out.printf("factorial of %d=%d ", number, factorial);
+	
+}
+}
+
+
+
